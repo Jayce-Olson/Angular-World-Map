482f20491b8fd968acc836e1cd0dbe28672a147e
Jayce <Jaycefo@gmail.com>
2024-10-28 08:52:14 -0600

    Task C

---
+++ src/app/info/info.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.css'
+})
+export class InfoComponent {
+  @Input() data!: {
+    name: string;
+    capital: string;
+    incomeLevel: string;
+    region: string;
+    longitude: number;
+    latitude: number;
+  };
+}
